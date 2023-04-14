55997f63f8034a3e581f779c583c93fbe3ad41c0
soumya-b019 <100502907+soumya-b019@users.noreply.github.com>
2023-04-14 23:57:14 +0530

    Update SearchResultsList.jsx

--- src/components/SearchResultsList.jsx
+++ src/components/SearchResultsList.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import './SearchResultsList.css'
 import SearchResult from './SearchResult';
 
-const SearchResultsList = (props) => {
-    const {results} = props;
+const SearchResultsList = ({results}) => {
 
     return (
         <div className='results-list'>
